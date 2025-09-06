ecd421d906175fe0bf96b7bbe4508e229bb26d50
carlosbetancourt7 <betancourtjrc@gmail.com>
2025-09-06 19:05:19 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "#85008bff"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,25 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
+    // toggleGrid();
 
 
     // TODO 2 - Create Platforms
-    createPlatform(200, 650, 200, 20);
-    createPlatform(650, 650, 200, 20);
-    createPlatform(1100, 650, 200, 20);
-    createPlatform(425, 525, 200, 20);
-    createPlatform(880, 525, 200, 20);
-    createPlatform(650, 400, 200, 20);
-
+    createPlatform(200, 650, 200, 20, "#85008bff");
+    createPlatform(650, 650, 200, 20, "#85008bff");
+    createPlatform(1100, 650, 200, 20, "#85008bff");
+    createPlatform(425, 525, 200, 20, "#bc08adff");
+    createPlatform(880, 525, 200, 20, "#bc08adff");
+    createPlatform(650, 400, 200, 20, "#f229deff");
+    createPlatform(1100, 400, 200, 20, "#f229deff");
+    createPlatform(1305, 525, 200, 20, "#bc08adff");
+    createPlatform(1250, 300, 20, 100, "#f229deff");
+    createPlatform(200, 400, 200, 20, "#f229deff");
+    createPlatform(1100, 200, 100, 20, "#eb62b9ff");
     // TODO 3 - Create Collectables
-    createCollectable("max", 730, 600, 0.025, 1);
-
-    
+    createCollectable("candy", 730, 600, 0.025, 1);
+    createCollectable("diamond2", 1335, 475, 0.025, 1);
+    createCollectable("money", 730, 350, 0.025, 1);
+    createCollectable("sombrero", 1241, 250, 0.025, 1);
+    createCollectable("vase", 1130, 150, 0.025, 1);
     // TODO 4 - Create Cannons
     createCannon("right", 650, 1050);
-    
-    
+    createCannon("left", 425, 1050);
+    createCannon("left", 100, 1100);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -98,4 +98,9 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  candy: { image: "images/collectables/candy.png" },
+  diamond2: { image: "images/collectables/diamond (1).png" },
+  vase: { image: "images/collectables/fancy-vase.png" },
+  money: { image: "images/collectables/bag-of-money.png" },
+  sombrero: { image: "images/collectables/sombrero (1).png"}
 };

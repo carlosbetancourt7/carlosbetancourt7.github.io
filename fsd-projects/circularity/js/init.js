47a9d40be11f0b683d5500f05a31481bd1d3a9a4
carlosbetancourt7 <betancourtjrc@gmail.com>
2025-09-30 18:23:53 +0000

    Made it to TODO 3

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,16 +20,23 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
-
+        function drawCircle () {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2)
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 : Call the drawCircle() function
-
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
         // TODO 7 : Use a loop to create multiple circles
 
